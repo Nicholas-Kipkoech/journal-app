3f5dc4d7693a95422934163a5e1d20ba9f6dc204
Nicholas Kipkoech <nickey968@gmail.com>
2025-03-19 06:26:27 +0300

    add server actions for the api

--- app/(services)/api.ts
+++ app/(services)/api.ts
@@ -10,3 +10,24 @@ export const loginUser = async (email: string, password: string) => {
 
   return response.json();
 };
+
+interface Journal {
+  title: string;
+  content: string;
+  collectionId: string;
+  userId: string;
+  moodQuery: string;
+  mood: string;
+}
+
+export const createJournal = async (journal: Journal) => {
+  const response = await fetch("/api/journal/create", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(journal),
+  });
+  if (!response.ok) {
+    throw new Error("Failed to create journal");
+  }
+  return response.json();
+};
