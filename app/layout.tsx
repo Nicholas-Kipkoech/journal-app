89d25c67e5c827e41c265f29eb163d75e87040b6
Nicholas Kipkoech <nickey968@gmail.com>
2025-03-22 11:40:38 +0300

    add metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,14 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import ClientProviders from "./ClientProviders";
+import { Metadata } from "next";
 
 const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Journal App - Reflect on your life",
+  description: "Senior engineer interview",
+};
 
 export default function RootLayout({
   children,
