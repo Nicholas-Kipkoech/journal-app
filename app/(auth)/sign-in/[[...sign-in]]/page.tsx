57f2e6989080a89ba2d09e076f25024964a80e92
Nicholas Kipkoech <nickey968@gmail.com>
2025-03-23 18:20:07 +0300

    added gradient to auth pages

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -51,73 +51,75 @@ const SignIn = () => {
 
   return (
     <div className="flex flex-col md:flex-row items-center justify-center p-4 gap-10">
-      <div className="hidden md:block w-1/2">
-        <Image
-          src="https://img.freepik.com/free-photo/colorful-overloaded-bullet-journal_23-2150248154.jpg?t=st=1742735876~exp=1742739476~hmac=560df9a4b4fb313021dcda8790aa52a389716f51a25e1c6a3222902894fbee67&w=740"
-          alt="Journal Wallpaper"
-          height={500}
-          width={500}
-          className="rounded-lg shadow-lg"
-        />
-      </div>
-      <div className="w-1/2">
-        <span className="flex justify-center font-bold text-[1.8rem] my-2">
-          Sign In
-        </span>
+      <div className="flex flex-col md:flex-row gap-10  items-center p-4 md:shadow-xl rounded-md">
+        <div className="hidden md:block md:w-1/2 ">
+          <Image
+            src="https://img.freepik.com/free-photo/colorful-overloaded-bullet-journal_23-2150248154.jpg?t=st=1742735876~exp=1742739476~hmac=560df9a4b4fb313021dcda8790aa52a389716f51a25e1c6a3222902894fbee67&w=740"
+            alt="Journal Wallpaper"
+            height={500}
+            width={500}
+            className="rounded-lg shadow-lg"
+          />
+        </div>
+        <div className=" w-full md:w-1/2 flex flex-col items-center md:bg-gradient-to-b from-[#32a2a8] to-white rounded-md md:p-5 p-2">
+          <span className="flex justify-center font-bold text-[1.8rem] my-2">
+            Sign In
+          </span>
 
-        <form
-          className="flex flex-col gap-4 border p-10 mx-auto rounded-md shadow-2xl"
-          onSubmit={handleSubmit}
-        >
-          <div>
-            <label>Email</label>
-            <Input
-              type="email"
-              placeholder="Email"
-              required
-              value={request.email}
-              onChange={(e) =>
-                setRequest({ ...request, email: e.target.value })
-              }
-              className="w-[20rem] h-[2.5rem]"
-            />
-          </div>
-          <div>
-            <label>Password</label>
-            <Input
-              type={showPassword ? "text" : "password"}
-              required
-              value={request.password}
-              onChange={(e) =>
-                setRequest({ ...request, password: e.target.value })
-              }
-              placeholder="Password"
-              className="w-[20rem] h-[2.5rem]"
-            />
-          </div>
-          <div className="flex items-center gap-2">
-            <input
-              type="checkbox"
-              checked={showPassword}
-              onChange={() => setShowPassword(!showPassword)}
-            />
-            <p className="text-[13px]">Show password</p>
-          </div>
+          <form
+            className="flex flex-col gap-4 p-4 md:p-10  rounded-md w-full"
+            onSubmit={handleSubmit}
+          >
+            <div>
+              <label>Email</label>
+              <Input
+                type="email"
+                placeholder="Email"
+                required
+                value={request.email}
+                onChange={(e) =>
+                  setRequest({ ...request, email: e.target.value })
+                }
+                className="w-full h-[2.5rem] bg-white"
+              />
+            </div>
+            <div>
+              <label>Password</label>
+              <Input
+                type={showPassword ? "text" : "password"}
+                required
+                value={request.password}
+                onChange={(e) =>
+                  setRequest({ ...request, password: e.target.value })
+                }
+                placeholder="Password"
+                className="w-full h-[2.5rem] bg-white"
+              />
+            </div>
+            <div className="flex items-center gap-2">
+              <input
+                type="checkbox"
+                checked={showPassword}
+                onChange={() => setShowPassword(!showPassword)}
+              />
+              <p className="text-[13px]">Show password</p>
+            </div>
 
-          <Button type="submit" variant={"journal"}>
-            {loading ? "Logging in..." : "Continue"}
-          </Button>
+            <Button type="submit" variant={"journal"}>
+              {loading ? "Logging in..." : "Continue"}
+            </Button>
 
-          <div className="flex text-[13px] justify-center gap-1">
-            <p>Dont have an account yet?</p>
-            <Link
-              href={"/sign-up"}
-              className="text-blue-600 font-bold cursor-pointer"
-            >
-              Sign Up
-            </Link>
-          </div>
-        </form>
+            <div className="flex text-[13px] justify-center gap-1">
+              <p>Dont have an account yet?</p>
+              <Link
+                href={"/sign-up"}
+                className="text-blue-600 font-bold cursor-pointer"
+              >
+                Sign Up
+              </Link>
+            </div>
+          </form>
+        </div>
       </div>
     </div>
   );
--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -31,82 +31,85 @@ const SignUp = () => {
   }
 
   return (
-    <div className="flex flex-col md:flex-row items-center justify-center p-4 gap-10">
-      {/* Image Section */}
-      <div className="hidden md:block w-1/2">
-        <Image
-          src="https://img.freepik.com/free-photo/colorful-overloaded-bullet-journal_23-2150248154.jpg?t=st=1742735876~exp=1742739476~hmac=560df9a4b4fb313021dcda8790aa52a389716f51a25e1c6a3222902894fbee67&w=740"
-          alt="Journal Wallpaper"
-          height={500}
-          width={500}
-          className="rounded-lg shadow-lg"
-        />
-      </div>
+    <div className="flex flex-col md:flex-row items-center justify-center">
+      <div className="flex flex-col md:flex-row  gap-10 md:shadow-xl p-4 rounded-md">
+        {/* Image Section */}
 
-      {/* Form Section */}
-      <div className="w-full md:w-1/2 flex flex-col items-center">
-        <span className="font-bold text-[1.8rem] my-2">Sign Up</span>
+        <div className="hidden md:block w-1/2">
+          <Image
+            src="https://img.freepik.com/free-photo/colorful-overloaded-bullet-journal_23-2150248154.jpg?t=st=1742735876~exp=1742739476~hmac=560df9a4b4fb313021dcda8790aa52a389716f51a25e1c6a3222902894fbee67&w=740"
+            alt="Journal Wallpaper"
+            height={500}
+            width={500}
+            className="rounded-lg shadow-lg"
+          />
+        </div>
 
-        <div className="flex flex-col gap-4 border p-10 rounded-md shadow-2xl w-full max-w-[400px]">
-          <div>
-            <label>First Name</label>
-            <Input
-              value={request.firstName}
-              onChange={(e) =>
-                setRequest({ ...request, firstName: e.target.value })
-              }
-              placeholder="First Name"
-              className="w-full h-[2.5rem]"
-            />
-          </div>
-          <div>
-            <label>Last Name</label>
-            <Input
-              placeholder="Last Name"
-              className="w-full h-[2.5rem]"
-              value={request.lastName}
-              onChange={(e) =>
-                setRequest({ ...request, lastName: e.target.value })
-              }
-            />
-          </div>
-          <div>
-            <label>Email</label>
-            <Input
-              type="email"
-              placeholder="Email"
-              className="w-full h-[2.5rem]"
-              value={request.email}
-              onChange={(e) =>
-                setRequest({ ...request, email: e.target.value })
-              }
-            />
-          </div>
-          <div>
-            <label>Password</label>
-            <Input
-              type="password"
-              placeholder="Password"
-              className="w-full h-[2.5rem]"
-              value={request.password}
-              onChange={(e) =>
-                setRequest({ ...request, password: e.target.value })
-              }
-            />
-          </div>
+        {/* Form Section */}
+        <div className="w-full md:w-1/2 flex flex-col items-center md:bg-gradient-to-b from-[#32a2a8] to-white rounded-md">
+          <span className="font-bold text-[1.8rem] my-2">Sign Up</span>
 
-          <Button variant={"journal"} onClick={handleRegisterUser}>
-            {loading ? "Signing up..." : "Continue"}
-          </Button>
+          <div className="flex flex-col gap-4  p-10 rounded-md  w-full max-w-[400px]">
+            <div>
+              <label>First Name</label>
+              <Input
+                value={request.firstName}
+                onChange={(e) =>
+                  setRequest({ ...request, firstName: e.target.value })
+                }
+                placeholder="First Name"
+                className="w-full h-[2.5rem] bg-white"
+              />
+            </div>
+            <div>
+              <label>Last Name</label>
+              <Input
+                placeholder="Last Name"
+                className="w-full h-[2.5rem] bg-white"
+                value={request.lastName}
+                onChange={(e) =>
+                  setRequest({ ...request, lastName: e.target.value })
+                }
+              />
+            </div>
+            <div>
+              <label>Email</label>
+              <Input
+                type="email"
+                placeholder="Email"
+                className="w-full h-[2.5rem] bg-white"
+                value={request.email}
+                onChange={(e) =>
+                  setRequest({ ...request, email: e.target.value })
+                }
+              />
+            </div>
+            <div>
+              <label>Password</label>
+              <Input
+                type="password"
+                placeholder="Password"
+                className="w-full h-[2.5rem] bg-white"
+                value={request.password}
+                onChange={(e) =>
+                  setRequest({ ...request, password: e.target.value })
+                }
+              />
+            </div>
 
-          <div className="flex justify-center text-[13px] gap-1">
-            <p>Already have an account?</p>
-            <Link
-              href={"/sign-in"}
-              className="cursor-pointer text-blue-600 font-bold"
-            >
-              Sign In
-            </Link>
+            <Button variant={"journal"} onClick={handleRegisterUser}>
+              {loading ? "Signing up..." : "Continue"}
+            </Button>
+
+            <div className="flex justify-center text-[13px] gap-1">
+              <p>Already have an account?</p>
+              <Link
+                href={"/sign-in"}
+                className="cursor-pointer text-blue-600 font-bold"
+              >
+                Sign In
+              </Link>
+            </div>
           </div>
         </div>
       </div>
