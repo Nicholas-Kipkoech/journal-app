f1aa12ee0bda02ee10b27a817f90c8eb1bf7b899
Nicholas Kipkoech <nickey968@gmail.com>
2025-03-23 16:28:45 +0300

    update screen layout to remove scroll

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${inter.className} antialiased`}>
         <ClientProviders>
-          <main className="min-h-screen">{children}</main>
+          <main className="max-h-screen">{children}</main>
         </ClientProviders>
       </body>
     </html>
